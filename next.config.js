3d60446e5d71f7441bf41a9adcddcce313fde83f
Ian Connor <iconnor@projectlounge.com>
2023-11-09 11:24:04 +1000

    Revert changes

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'blissaiptyltd-502080171.imgix.net',
-  },
-}
-
+module.exports = nextConfig
