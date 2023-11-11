204a133a79e197f4f16ab64d426b601250d44f84
Ian Connor <iconnor@projectlounge.com>
2023-11-11 17:35:43 +1000

    Update icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,20 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Powston',
   description: 'We are focused on innovative energy solutions and sustainability.',
+  icons: [
+    {
+      rel: 'icon',
+      url: '/favicon.ico',
+    },
+    {
+      rel: 'apple-touch-icon',
+      url: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'manifest',
+      url: '/site.webmanifest',
+    },
+  ],
 }
 
 export default function RootLayout({
