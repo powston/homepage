770fc59a600c195d6152b23fe711034bcb93c2e5
Ian Connor <iconnor@projectlounge.com>
2023-11-22 17:01:00 +1000

    Add hardware images and NET logos

--- app/page.tsx
+++ app/page.tsx
@@ -75,12 +75,133 @@ export default function Home() {
         </div>
       </section>
 
+      <section id="product" className="product py-20">
+        <div className="container mx-auto px-4">
+
+        <div className="flex flex-wrap -mx-4">
+            <div className="w-full md:w-1/3 px-4 mb-4 md:mb-0">
+              <div className="py-8 h-full text-center">
+                <i className="fas fa-envelope text-blue-500 mb-2"></i>
+                <h4 className="text-white text-lg uppercase">Product Development</h4>
+                <hr className="my-4 border-white" />
+                <div className="text-white text-opacity-50">
+                  <div className="grid grid-cols-3 gap-4 mt-10">
+                  <div className="text-white text-opacity-50">
+                      <Image
+                        src="/images/hardware.jpg"
+                        alt="Example prototype product"
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                    <div className="text-white text-opacity-50">
+                      <Image
+                        src="/images/hardware1.jpg"
+                        alt="Testing prototype product"
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                    <div className="text-white text-opacity-50">
+                      <Image
+                        src="/images/hardware2.jpg"
+                        alt="Testing prototype product"
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                    <div className="text-white text-opacity-50">
+                      <Image
+                        src="/images/hardware3.jpg"
+                        alt="Testing prototype product"
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                    <div className="text-white text-opacity-50">
+                      <Image
+                        src="/images/hardware4.jpg"
+                        alt="Testing prototype product"
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                    <div className="text-white text-opacity-50">
+                      <Image
+                        src="/images/hardware5.jpg"
+                        alt="Testing prototype product"
+                        width={500}
+                        height={500}
+                      />
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full md:w-2/3 px-4">
+                <div className="py-8 h-full text-center">
+                  <i className="fas fa-mobile-alt text-blue-500 mb-2"></i>
+                  <h4 className="text-white text-lg uppercase">Performance Metrics</h4>
+                  <hr className="my-4 border-white" />
+                  <div className="text-white text-opacity-50">
+                    <Image
+                      alt="Comparison of energy cost over a month"
+                      src="/images/roi.png"
+                      width={1000}
+                      height={500}
+                    />
+                  </div>
+                  <div className="flex p-2">
+                    <div className="w-3/4 pr-2">
+                      <div className="text-white text-opacity-50">
+                        <Image
+                          alt="Comparison of energy cost over a month"
+                          src="/images/roi1.jpg"
+                          width={1212}
+                          height={226}
+                        />
+                      </div>
+                    </div>
+                    <div className="w-1/4">
+                      <div className="text-white text-opacity-50">
+                        <Image
+                          alt="Comparison of energy cost over a month"
+                          src="/images/roi2.jpg"
+                          width={1370}
+                          height={652}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <img
+            className="mt-4 mx-auto"
+            width={650}
+            src="https://s3.ap-southeast-2.amazonaws.com/assets.newenergytech.org.au/uploads/NETCC-Features-01.png"
+            alt="Approved Seller Logo"
+          />
+          </div>
+
+        </div>
+      </section>
+
 
       <footer className="py-20 bg-black">
         <div className="container mx-auto px-4 text-center text-white text-opacity-50">
-          Copyright &copy; BlissAI Pty Ltd 2023
+          Copyright &copy; BlissAI Pty Ltd is a proud New Energy Tech Approved Seller committed to meeting consumer protection standards throughout your customer journey with us.
+          <a href="https://www.newenergytech.org.au/find-an-approved-seller" target="_blank" rel="noopener noreferrer">
+          <img
+            className="mt-4 mx-auto"
+            width={150}
+            src="https://s3.ap-southeast-2.amazonaws.com/assets.newenergytech.org.au/uploads/New-Energy-Tech-Approved-Seller-Logo.svg"
+            alt="Approved Seller Logo"
+          />
+          </a>
         </div>
       </footer>
+
 
     </>
   )
